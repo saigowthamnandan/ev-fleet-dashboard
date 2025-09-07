6c6c3e0ba501a18160322593e05b2873454b28ba
Gowtham Nandan <venkatgowtham54321@gmail.com>
2025-09-07 16:52:26 +0200

    fix: updates translations route with lint

--- src/app/api/translations/route.ts
+++ src/app/api/translations/route.ts
@@ -1,13 +1,24 @@
 import { translations } from "./translations";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(request: NextRequest, response: NextResponse) {
-  const module = request.nextUrl.searchParams.get("module"), locale = request.nextUrl.searchParams.get("locale");
-  if (typeof module === "string" && typeof locale === "string" && module && locale) {
+export async function GET(request: NextRequest) {
+  const module = request.nextUrl.searchParams.get("module");
+  const locale = request.nextUrl.searchParams.get("locale");
+
+  if (
+    typeof module === "string" &&
+    typeof locale === "string" &&
+    module &&
+    locale
+  ) {
     const content = translations.find((t) => t.language === locale)?.content;
     if (content) {
-      return new Response(JSON.stringify({...content.common, ...content[module]}),{status: 200});
+      return new Response(
+        JSON.stringify({ ...content.common, ...content[module] }),
+        { status: 200 }
+      );
     }
   }
+
   return new Response(null, { status: 404 });
 }
